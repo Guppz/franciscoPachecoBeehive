0f1f621cd2802c40781fe0f25f4a305e4dd4df63
Francisco <Franc-900@hotmail.com>
2020-05-08 00:24:36 -0600

    manifest

---
+++ public/js/serviceWorker/serviceWorker.js
@@ -0,0 +1,12 @@
+const cacheName = 'v2';
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+});
+
